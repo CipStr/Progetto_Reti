7e172da51d028048d3eac61e32d76750b7cb8446
Ciprian <ciprianstricescu@gmail.com>
2021-05-29 18:28:38 +0200

    Added UDP clients + UDP server + UDP server multithread

---
+++ client_UDP_3.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 29 18:20:21 2021
+
+@author: User
+"""
+
+import socket
+from datetime import datetime
+
+def dati():
+
+    lista = ['192.168.1.3', datetime.now().strftime("%H:%M:%S"), 32, 10]
+    return lista
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = ('localhost', 10000)
+
+try:
+    
+    file = open("Prova_iot.txt", "w+")
+    lista = dati()
+    file.write(str(dati()))
+    file.seek(0)
+    sent = socket.sendto(file.read().encode(), server_address)
+    file.close()
+    data, server = socket.recvfrom(4096)
+    print ('received message "%s"' % data.decode('utf8'))
+except Exception as info :
+    print(info)
+finally:
+    socket.close()
